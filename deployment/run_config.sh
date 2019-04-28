9c8cbc2a69f2b74b9e1f906d2625257c9f71b107
mysite <huangsite378@gmail.com>
2019-04-28 21:11:00 +1000

    apache setup -- test html

--- deployment/run_config.sh
+++ deployment/run_config.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-. ./openrc.sh; ansible-playbook -i hosts -u ubuntu --key-file=testKeypair.pem playbook.yml
+. ./openrc.sh; ansible-playbook -i hosts -u ubuntu --key-file=testKeypair.pem docker_install.yml
