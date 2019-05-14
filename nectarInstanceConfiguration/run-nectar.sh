31b854b10f9efb1b6f77306171e51dfb5c3346fa
Eric9395 <39228269+Eric9395@users.noreply.github.com>
2019-05-14 15:01:05 +0930

    Add files via upload

---
+++ nectarInstanceConfiguration/run-nectar.sh
@@ -0,0 +1,12 @@
+# Team 20
+# Team member: Site Huang, 908282
+#              Chenyuan Zhang, 815901
+#              Zixuan Zhang, 846305
+#              Zhentao Zhang, 864735
+#              Kangyun Dou, 740145
+#!/bin/bash
+
+. ./unimelb-comp90024-group-20-openrc.sh; ansible-playbook --ask-become-pass nectar.yaml
+
+
+# MTQyODY0MGExOTY3ZDhj
